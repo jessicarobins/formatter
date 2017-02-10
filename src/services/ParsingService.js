78bda69805d2f4784eac32cd3b429434e635feb8
Jess <jessrrobins@gmail.com>
2017-02-10 03:52:21 +0000

    we are sort of parsing

--- src/services/ParsingService.js
+++ src/services/ParsingService.js
@@ -1,18 +1,83 @@
 import Vue from 'vue'
+import * as _ from 'lodash'
 
 export default {
   parse(text) {
-    return this.parseLine(text.split('\n'));
+    const textArray = text.split('\n')
+    return this.parseLine(textArray);
   },
   
   parseLine(
     textArray,
-    parent_id=null,
+    json=[],
+    parent=null,
     depth=0,
-    previous=null,
-    errorCount=0,
-    specCount=0) {
-      
-    return textArray;
+    previous=null) {
+    
+    const regex = /(-*)([^-#]+)\s?(.*)/
+    
+    if (textArray.length < 1) {
+      return json
+    }
+        
+    let line = textArray[0]
+    //this is where we turn spaces and tabs into -s
+    line = line.replace(/[ ]{2}/, '-')
+    line = line.replace(/\t/, '-')
+        
+    const resultArray = line.match(regex)
+    const tabs = resultArray[1]
+    const description = resultArray[2]
+    
+    const specDepth = tabs.length ? tabs.length : 0
+        
+    let spec = {
+      description: description,
+      children: []
+    }
+    
+    if(depth === 0){
+      if(parent) {
+        parent.children.push(spec)
+        spec.parent = parent
+      }
+      else {
+        json.push(spec)
+      }
+    }
+    else {
+      if(depth === specDepth) {
+        parent = previous ? previous.parent : undefined
+        spec.parent = parent
+        if (parent) {
+          parent.children.push(spec)
+        }
+      }
+      else if(specDepth > depth) {
+        previous.children.push(spec)
+        spec.parent = previous
+      }
+      // specDepth < depth
+      else {
+        for(let i=0; i < (depth-specDepth); i++) {
+          previous = previous.parent
+        }
+        parent = previous.parent
+        spec.parent = parent
+        if (parent) {
+          parent.children.push(spec)
+        }
+      }
+    }
+            
+    textArray = _.tail(textArray)
+        
+    this.parseLine(   
+        textArray,
+        json,
+        parent,
+        specDepth, 
+        spec,
+        parent)
   }
 }
