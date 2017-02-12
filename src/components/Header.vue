f181465951a80358652b19f353f4d3d102c6a0de
Jess <jessrrobins@gmail.com>
2017-02-12 16:50:01 +0000

    add a what is this button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app-header">
     <h2 class="md-display-4">ddescribe</h2>
+    <md-button @click.native="openDialog('help')" class="md-primary">What is this?</button>
+    <md-dialog-alert
+      :md-content-html="alert.content"
+      md-ok-text="Cool"
+      ref="help">
+    </md-dialog-alert>
   </div>
 </template>
 
@@ -10,7 +16,25 @@ export default {
   data () {
     return {
       alert: {
-        content: '<div>Type specs\n\tAdd children by hitting tab\n</div>'
+        content: `ddescribe solves a pretty simple, but mildly annoying problem: 
+        sometimes you want to go from indentation-formatted test cases to test framework 
+        code. Maybe a different team writes your test plans, or maybe you want 
+        to brainstorm the whole plan out before implementing - whatever the 
+        reason 
+        <ol>
+          <li>type/paste in test cases</li>
+          <li>select framework</li>
+          <li>submit</li>
+          <li>save 5 min</li>
+        </ol>`
+      }
+    }
+  },
+  methods: {
+    openDialog(ref) {
+      this.$refs[ref].open()
+      if (this.$ga) {
+        this.$ga.trackEvent('about', 'click')
       }
     }
   }
